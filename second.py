36736f0f8f480b178e6386bafbc4916e029548ac
Grace Pak <gracep.41448@redlandsschools.net>
2018-09-10 15:25:43 +0000

    second task not yet completed

---
+++ second.py
@@ -0,0 +1,17 @@
+"""
+Triple QUOTES will allow me to comment out multiple lines. 
+
+Write a program that will ask the user what their age is and then, 
+determine if they are old enough to vote or not and respond appropriately.
+
+"""
+
+age = input ("How old are you?")
+
+if age < 18:
+    print ("You're too young to vote. Wait a few more years.")
+
+else age > 18:
+    print ("You are old enough to vote. Who are you voting for?")
+    
+
