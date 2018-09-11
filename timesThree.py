555509904346d424ab3445e4d095e295055d9e17
Grace Pak <gracep.41448@redlandsschools.net>
2018-09-11 16:16:28 +0000

    new tasks

---
+++ timesThree.py
@@ -0,0 +1,7 @@
+"""
+Write a program that will take the user input and return that input times 3
+Make it work for decimals or integers
+"""
+
+x = float(input("Please type in a number: "))
+print ("The number " + str(x) + " times 3 is equal to: " + str(x*3))
