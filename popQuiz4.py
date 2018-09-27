6badfe82c698975e9b5dcb4d1f3217b03d9aa293
Grace Pak <gracep.41448@redlandsschools.net>
2018-09-27 14:44:45 +0000

    popQuiz4

--- popQuiz4.py
+++ popQuiz4.py
@@ -3,9 +3,10 @@ Write a program that defines a function called addSix(n):
 This program should take in a number from the user and then call the function to add six to that number, and then RETURN this number back.
 Use the necessary comments and print statements in your code.
 """
-def addSix():
-    n = int(input("Please enter a number and I'll add six to it: "))
-    print (n+6)
+def addSix(n):
+    print ("You're number " + str(n) + " plus six is " +str(n+6))
     return n
     
-addSix()
+n = int(input("Please enter a number and I'll add six to it: "))    
+ 
+addSix(n)
